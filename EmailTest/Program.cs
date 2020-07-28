7c7b4bd8434f2481ab8216136feec2cdb474e75d
Hotel2Oscar <waters.daniel.c@gmail.com>
2020-07-28 09:50:09 -0400

    Initial commit

---
+++ EmailTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.IO;
+
+namespace EmailTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            SendEmail();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        public static void SendEmail()
+        {
+            try
+            {
+                var outlook = new Application();
+
+                var email = (MailItem)outlook.CreateItem(OlItemType.olMailItem);
+
+                email.Body = "This is a programmatically generated email.";
+
+                var attachments = new string[] { "Program.cs", "Another Blue Abstract Desktop.jpg" };
+                
+                foreach(var file in attachments)
+                {
+                    var attachment = email.Attachments.Add(Path.Combine(Directory.GetCurrentDirectory(), file), OlAttachmentType.olByValue, email.Body.Length + 1, file);
+                }
+
+                email.Subject = "Test email";
+
+                var recipient = email.Recipients.Add("waters.daniel.c@gmail.com");
+                recipient.Resolve();
+
+                email.Send();
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
